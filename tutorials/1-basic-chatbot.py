a34042bb0055a0f9ed267428f60c0277870fcf67
Bjoern Winkler <bjoern@bjoern-winkler.de>
2025-05-29 19:25:49 +0200

    customize state

--- tutorials/1-basic-chatbot.py
+++ tutorials/1-basic-chatbot.py
@@ -13,6 +13,8 @@ from langgraph.types import Command, interrupt
 
 from langchain.chat_models import init_chat_model
 from langchain.tools import tool
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import InjectedToolCallId
 
 from langchain_tavily import TavilySearch
 
@@ -37,6 +39,8 @@ class State(TypedDict):
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
+    name: str
+    birthday: str
 
 
 # create memory
@@ -48,10 +52,40 @@ graph_builder = StateGraph(State)
 
 # define tools
 @tool
-def human_assistance(query: str) -> str:
+# Note that because we are generating a ToolMessage for a state update, we
+# generally require the ID of the corresponding tool call. We can use
+# LangChain's InjectedToolCallId to signal that this argument should not
+# be revealed to the model in the tool's schema.
+def human_assistance(
+    name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]
+) -> str:
     """Request assistance from a human."""
-    human_response = interrupt({"query": query})
-    return human_response["data"]
+    human_response = interrupt(
+        {
+            "question": "Is this correct?",
+            "name": name,
+            "birthday": birthday,
+        }
+    )
+    # if the information is correct, update the state as-is.
+    if human_response.get("correct", "").lower().startswith("y"):
+        verified_name = name
+        verified_birthday = birthday
+        response = "Correct"
+    else:
+        verified_name = human_response.get("name", name)
+        verified_birthday = human_response.get("birthday", birthday)
+        response = f"Made a correction: {human_response}"
+
+    # explicitely update the state with a ToolMessage inside
+    # the tool.
+    state_update = {
+        "name": verified_name,
+        "birthday": verified_birthday,
+        "messages": [ToolMessage(response, tool_call_id=tool_call_id)],
+    }
+    # return a Command object in the tool to update the state
+    return Command(update=state_update)
 
 
 search_tool = TavilySearch(max_results=2)
@@ -104,7 +138,8 @@ def stream_graph_updates(user_input: str):
 
 def main():
 
-    user_input = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
+    user_input = """Can you look up when LangGraph was released?
+        When you have the answer, use the human_assistance tool for review."""
     config = {"configurable": {"thread_id": "1"}}
 
     events = graph.stream(
@@ -112,6 +147,7 @@ def main():
         config,
         stream_mode="values",
     )
+
     for event in events:
         if "messages" in event:
             event["messages"][-1].pretty_print()
@@ -120,12 +156,12 @@ def main():
     print(snapshot)
     print(snapshot.next)
 
-    human_response = (
-        "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent."
-        " It's much more reliable and extensible than simple autonomous agents."
+    human_command = Command(
+        resume={
+            "name": "LangGraph",
+            "birthday": "Jan 17, 2024",
+        },
     )
-
-    human_command = Command(resume={"data": human_response})
 
     events = graph.stream(human_command, config, stream_mode="values")
     for event in events:
