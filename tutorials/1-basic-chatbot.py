af41b99608391ce4de0b27944fd6e3cdf4abf7e6
Bjoern Winkler <bjoern@bjoern-winkler.de>
2025-05-29 16:35:11 +0200

    Cleaned up tutorial 2 version

--- tutorials/1-basic-chatbot.py
+++ tutorials/1-basic-chatbot.py
@@ -38,51 +38,6 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-class BasicToolNode:
-    """A node that runs the tools requested in the last AIMessage."""
-
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No messages found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    too_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
-
-
-def route_tools(state: State):
-    """Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    print(f"Invoking route_tools. {state=}")
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in the input state to tool_edge: {state}")
-    # check if any tool_calls in the ai_message, then route to tools node
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        print("route_tools returning 'tools'.")
-        return "tools"
-    # route to the end otherwise
-    return END
-
-
 # construct the graph
 graph_builder = StateGraph(State)
 
@@ -92,14 +47,18 @@ tools = [tool]
 llm_with_tools = llm.bind_tools(tools)
 
 
+# define chatbot and add to graph
 def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
 
-# add a node for the tools and add to graph
-# tool_node = BasicToolNode(tools=[tool])
+# add a node for the tools and add as conditional edge to graph
+# using the prebuilt ToolNode and tools_condition,
+# this will route from the chatbot if the returned ai_message
+# contains a call to tools
 tool_node = ToolNode(tools=[tool])
 graph_builder.add_node("tools", tool_node)
 
@@ -107,22 +66,10 @@ graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-# The 'route_tools' function returns "tools" if the chatbot asks to use a tool, and 'END' if
-# it is fine directly responding. This conditional routing defines the main agent loop.
-# graph_builder.add_conditional_edges(
-#     "chatbot",
-#     route_tools,
-#     # dictionary tells the graph to interpret the condition functions
-#     # as specific nodes. You can use this if you want to provide a specific name
-#     # for 'tools', e.g. "tools": "my_tools"
-#     {"tools": "tools", END: END},
-# )
+
 # any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "chatbot")
 
-# add starting edge for entry
-# this will go from START to the chatbot
-graph_builder.add_edge(START, "chatbot")
 # compile the graph
 graph = graph_builder.compile()
 
